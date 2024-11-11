6684137ab8d7959d350c432737e7cd02812fda4a
auto-Dog <2969860272@qq.com>
2024-11-11 22:46:46 +0800

    debug sample func

--- train.py
+++ train.py
@@ -154,7 +154,7 @@ def validate(testloader, model, criterion, optimizer, lrsch, logger, args):
 
     return acc, model.state_dict()
 
-def sample_enhancement(model,inferenceloader,epoch):
+def sample_enhancement(model,inferenceloader,epoch,args):
     ''' 根据给定的图片，进行颜色优化
 
     目标： $argmax_{c_i} p(\hat{c}|I^{cvd}c_i^{cvd})$ 
@@ -187,6 +187,7 @@ def sample_enhancement(model,inferenceloader,epoch):
         loss.backward()
         inference_optimizer.step()
         if iter%10 == 0:
+            print(out.shape)    # debug
             print(f'Mean Absolute grad: {torch.mean(torch.abs(img_t.grad))}')
 
     # img_out = img_t.clone()
@@ -229,15 +230,15 @@ if args.test == True:
     finaltestset = CVDcifar('./',train=False,download=True)
     finaltestloader = torch.utils.data.DataLoader(finaltestset,batch_size=args.batchsize,shuffle = False,num_workers=8)
     model.load_state_dict(torch.load(pth_location, map_location='cpu'))
-    sample_enhancement(model,None,-1)
+    sample_enhancement(model,None,-1,args)
     # testing(finaltestloader,model,criterion,optimizer,lrsch,logger,args)
 else:
     for i in range(args.epoch):
         print("===========Epoch:{}==============".format(i))
-        # sample_enhancement(model,inferenceloader,i) # debug
+        sample_enhancement(model,None,i,args) # debug
         train(trainloader, model,criterion,optimizer,lrsch,logger,args,i)
         score, model_save = validate(valloader,model,criterion,optimizer,lrsch,logger,args)
-        sample_enhancement(model,None,i)
+        sample_enhancement(model,None,i,args)
         if score > auc:
             auc = score
             torch.save(model_save, pth_location)
