9de6905107f2cdcf48951cc370468ef7681ac52b
auto-Dog <2969860272@qq.com>
2024-11-29 17:31:07 +0800

    update running sh

---
+++ train_64_deu.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#SBATCH -o job.%j.out
+#SBATCH -N 1
+#SBATCH -c 8
+#SBATCH --gres=gpu:1
+
+python train.py --prefix s64_K3_b8_deu --size 64 --x_size "(3,64,64)" --y_size "(3,64,64)" --x_hidden_size 64 --cvd 'deutan'
