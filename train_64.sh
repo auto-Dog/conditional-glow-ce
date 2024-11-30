8da31480458a24f88fc750edf073833cb780a498
auto-Dog <2969860272@qq.com>
2024-12-01 01:54:37 +0800

    debug sample and train sh

--- train_64.sh
+++ train_64.sh
@@ -4,4 +4,4 @@
 #SBATCH -c 8
 #SBATCH --gres=gpu:1
 
-python train.py --prefix s64_K3_b8 --size 64 --x_size "(3,64,64)" --y_size "(3,64,64)" --x_hidden_size 64
+python train.py --prefix s64_K3_b8 --size 64 --x_size "(3,64,64)" --y_size "(3,64,64)" --x_hidden_size 64 --cvd protan
